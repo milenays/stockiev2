543fe1875c28b23b435f9e1bc6531b267efd1dfd
milenays <milenaystore@gmail.com>
2024-05-19 01:51:51 +0300

    asdf

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,8 @@ import { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
