56605e7b60d53a33d5dc630bec97fbab18978c9f
milenays <milenaystore@gmail.com>
2024-05-19 02:41:16 +0300

    asdfasdf

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
 const connectMongo = async () => {
-  if (mongoose.connections[0].readyState) return;
-  await mongoose.connect(process.env.MONGODB_URI, {
+  if (mongoose.connection.readyState >= 1) return;
+
+  return mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  console.log('Connected to MongoDB');
 };
 
 export default connectMongo;
