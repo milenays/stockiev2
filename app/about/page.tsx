854664f42c5696c3dcf85150885b2e5c504e0a42
milenays <milenaystore@gmail.com>
2024-05-19 01:53:24 +0300

    asdf

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+
 export default function About() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-green-500">About Stockie</h1>
+      <Link href="/" className="text-blue-500">Home</Link>
     </div>
   );
 }
