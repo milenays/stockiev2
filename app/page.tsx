2f386e43da9d46e285909cfa84c7aa65e3ac9d09
milenays <milenaystore@gmail.com>
2024-05-19 01:55:04 +0300

    asdasd

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 
-export default function Home() {
+export default function About() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Welcome to Stockie</h1>
-      <Link href="/about">
-        <a className="text-blue-500">About</a>
+      <h1 className="text-4xl font-bold text-green-500">About Stockie</h1>
+      <Link href="/" legacyBehavior>
+        <a className="text-blue-500">Home</a>
       </Link>
     </div>
   );
