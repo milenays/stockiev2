b11a5690cd6d0ba6bd13e03ec3e635a4c89df1ab
milenays <milenaystore@gmail.com>
2024-05-19 11:49:14 +0300

    asd

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,10 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import type { Config } from 'postcss';
 
-module.exports = {
+const config: Config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
 
+export default config;
