2b29cdf88bb20973ed4449f58085bbe6a5a721d7
milenays <milenaystore@gmail.com>
2024-05-19 11:09:44 +0300

    asdfasdf

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
-import { Config } from 'tailwindcss';
+import { defineConfig } from 'tailwindcss'
 
-const config: Config = {
+export default defineConfig({
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class', // Dark mode kullanımı
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
-export default config;
+})
