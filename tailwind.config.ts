849272d62c8ca1e6a30c4a757e447993bc0a1799
milenays <milenaystore@gmail.com>
2024-05-19 11:34:08 +0300

    sdf

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,12 @@ import { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}', 
-    './pages/**/*.{js,ts,jsx,tsx}', 
-    './components/**/*.{js,ts,jsx,tsx}'
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}', // Kendi dizinlerinizi ekleyin
   ],
+  darkMode: 'class', // Dark mode'u etkinleştirir
   theme: {
     extend: {},
   },
