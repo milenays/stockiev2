1f398679d4845b413d0647d609e018e89684e064
milenays <milenaystore@gmail.com>
2024-05-19 12:54:53 +0300

    sdsdf

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
-// next.config.mjs
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve.alias['@'] = __dirname;
     return config;
+  },
+  env: {
+    API_KEY: process.env.API_KEY,
+    API_SECRET: process.env.API_SECRET,
+    SELLER_ID: process.env.SELLER_ID,
   },
 };
 
